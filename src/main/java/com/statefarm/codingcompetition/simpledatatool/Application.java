9bf9801d13b80c644ef594f2d4bbe7849a03ce88
Will <Will@LAPTOP-OTQOQV7N>
2023-09-23 19:59:11 -0500

    test

--- src/main/java/com/statefarm/codingcompetition/simpledatatool/Application.java
+++ src/main/java/com/statefarm/codingcompetition/simpledatatool/Application.java
@@ -3,5 +3,6 @@ package com.statefarm.codingcompetition.simpledatatool;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        //test
     }
 }
